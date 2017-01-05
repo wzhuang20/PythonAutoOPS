9786de2549b4bd2f43cf04fd0a40ee19b2ed79e7
lenovo-PC\lenovo <wzhuang20@126.com>
2017-01-05 15:32:49 +0800

    第2章 业务服务监控详解

--- 2servermon/simple1.py
+++ 2servermon/simple1.py
@@ -1,2 +1,10 @@
 #--coding:UTF-8--
-import
+import difflib
+
+text1 = """text1:
+This module provides classes and functions for comparing sequences.
+including HTML and context and unified diffs.
+difflib document v7.4
+add string
+"""  # 定义字符串1
+text1_lines = text1.splitlines()  # 以行进行分割，以便进行对比
