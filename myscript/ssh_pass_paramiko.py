29789b46dbd908c800cbdb2cb85a43f9c19c7cf3
lenovo-PC\lenovo <wzhuang20@126.com>
2017-02-04 14:51:07 +0800

    ssh使用密码方式认证，通过exec_command()执行命令

--- myscript/ssh_pass_paramiko.py
+++ myscript/ssh_pass_paramiko.py
@@ -1,13 +1,13 @@
 # --coding:UTF-8--  需要crypto、ecdsa包和python开发包python-devel的支持
 import paramiko
 
-hostname='47.93.87.52'
+hostname='47.93.87.11'
 username='root'
 password='*********'
 paramiko.util.log_to_file('syslogin.log')  # 发送paramiko日志到syslogin.log文件
 
 ssh=paramiko.SSHClient()  # 创建一个ssh客户端client对象
-ssh.load_system_host_keys()  # 获取客户端host_keys，默认 ~/.ssh/known_hosts，非默认路径需要制定
+ssh.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 自动添加主机名及主机秘钥到本地hostkeys对象，并将其保存，不依赖load_system_host_keys()的配置，即使~/.ssh/known_hosts不存在也不产生影响
 
 ssh.connect(hostname=hostname,username=username,password=password)  # 创建ssh连接
 stdin,stdout,stderr=ssh.exec_command('free -m')  # 调用远程执行命令方法stdout.readlines()
